9ce874830dc6ea7d98eb635faf0db8f4ca87eb1b
Bjørn Tore Håvie <itsbth@itsbth.com>
2024-12-01 16:57:37 +0100

    Simplifications and refactorings

--- Day1.fs
+++ Day1.fs
@@ -3,20 +3,21 @@ module Day1
 open System.IO
 
 let solve =
-    let (a1, a2) =
+    let a1, a2 =
         File.ReadAllLines("input/day1.txt")
         |> Array.map (fun l -> l.Split "   " |> (fun a -> (int a.[0], int a.[1])))
         |> Array.unzip
 
-    let (a1, a2) = (a1 |> Array.sort, a2 |> Array.sort)
+    let a1, a2 = a1 |> Array.sort, a2 |> Array.sort
 
     printf "part 1: %d\n" (Array.zip a1 a2 |> Array.sumBy (fun (a, b) -> abs (a - b)))
 
-    let counts =
-        (a2
-         |> Array.fold (fun m v -> Map.change v (Option.defaultValue 0 >> fun c -> Some(c + 1)) m) Map.empty)
+    let counts = (a2 |> Array.countBy id |> Map.ofArray)
 
     printf
         "part 2: %d\n"
         (a1
-         |> Array.sumBy (fun v1 -> v1 * (counts |> Map.tryFind v1 |> Option.defaultValue 0)))
+         |> Array.sumBy (fun v1 ->
+             match counts |> Map.tryFind v1 with
+             | Some v2 -> v1 * v2
+             | None -> 0))
