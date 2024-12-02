7121110f3fc305bce8f5b81e30172889706dacda
Bjørn Tore Håvie <itsbth@itsbth.com>
2024-12-02 18:39:13 +0100

    Refactor Day2 module and move utility functions to Utils module; enhance readability and maintainability

--- Day2.fs
+++ Day2.fs
@@ -2,13 +2,8 @@ module Day2
 
 open System
 open System.IO
-
+open Utils
 open FSharpx.Prelude
-
-let inrange l u v = l <= v && v <= u
-let complement f = fun x -> not (f x)
-
-let countp p = Seq.filter p >> Seq.length
 
 let diff a =
     Seq.windowed 2 a |> Seq.map (fun a -> a.[1] - a.[0])
@@ -16,13 +11,7 @@ let diff a =
 let valid a =
     Seq.forall (inrange 1 3) a || Seq.forall (inrange -3 -1) a
 
-let skipi idx (sequence: seq<'a>) =
-    sequence
-    |> Seq.mapi (fun i x -> if i = idx then None else Some x)
-    |> Seq.choose id
-
 let valid2 (a: int array) =
-    // correctness by accident: seq { .. } is inclusive, so we end up including the index past the last element, therefore also testing the whole array
     seq { 0 .. (Array.length a) } |> Seq.exists ((flip skipi) a >> diff >> valid)
 
 let solve () : int option * int option =
--- Utils.fs
+++ Utils.fs
@@ -1,13 +1,19 @@
 module Utils
 
 /// Calculate the absolute difference between two numbers.
-///
-/// # Arguments
-///
-/// * `a` - The first number.
-/// * `b` - The second number.
-///
-/// # Returns
-///
-/// The absolute difference between `a` and `b`.
 let absoluteDifference (a, b) = abs (a - b)
+
+/// Check if a value is within an inclusive range
+let inrange l u v = l <= v && v <= u
+
+/// Create complement of a predicate function
+let complement f = fun x -> not (f x)
+
+/// Count elements in a sequence that satisfy a predicate
+let countp p = Seq.filter p >> Seq.length
+
+/// Skip an element at specified index in a sequence
+let skipi idx (sequence: seq<'a>) =
+    sequence
+    |> Seq.mapi (fun i x -> if i = idx then None else Some x)
+    |> Seq.choose id
