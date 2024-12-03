0d6822942084246ba9c51f1f1581fdc1af73710d
Bjørn Tore Håvie <itsbth@itsbth.com>
2024-12-03 14:15:20 +0100

    Reformat Program.fs

--- Program.fs
+++ Program.fs
@@ -2,31 +2,29 @@
 
 type solver = unit -> int option * int option
 
-let solvers = Map [
-    (1, Day1.solve)
-    (2, Day2.solve)
-    (3, Day3.solve)
-]
+let solvers = Map [ (1, Day1.solve); (2, Day2.solve); (3, Day3.solve) ]
 
 [<EntryPoint>]
 let main argv =
-    let day = 
+    let day =
         match argv with
-        | [|d|] -> 
+        | [| d |] ->
             match tryParse d with
             | Some n when solvers.ContainsKey(n) -> n
             | _ -> solvers.Keys |> Seq.max
         | _ -> solvers.Keys |> Seq.max
 
     match Map.tryFind day solvers with
-    | Some solve -> 
+    | Some solve ->
         printfn $"Running solver for day %d{day}"
-        let part1, part2 = solve()
+        let part1, part2 = solve ()
+
         match part1, part2 with
         | Some p1, Some p2 -> printfn $"Part 1: %d{p1}\nPart 2: %d{p2}"
         | Some p1, None -> printfn $"Part 1: %d{p1}"
         | None, Some p2 -> printfn $"Part 2: %d{p2}"
         | None, None -> printfn "No solution found"
+
         0
     | None ->
         printfn $"No solver found for day %d{day}"
