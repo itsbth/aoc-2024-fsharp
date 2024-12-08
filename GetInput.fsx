e6bb35aee3bd8921b43548003126267126e0fba5
Bjørn Tore Håvie <itsbth@itsbth.com>
2024-12-08 20:26:02 +0100

    Enhance input fetching with status handling and command-line arguments

--- GetInput.fsx
+++ GetInput.fsx
@@ -9,31 +9,60 @@ let session = File.ReadAllText ".session"
 
 let httpClient = new HttpClient()
 
-let fetchInput day =
+type status =
+    | Success
+    | Exists
+    | Error of string
+
+let fetchInput day force =
     task {
         let url = $"https://adventofcode.com/2024/day/{day}/input"
         let path = $"./input/day{day}.txt"
-        use req = new HttpRequestMessage(HttpMethod.Get, url)
-        req.Headers.Add("Cookie", $"session={session}")
-        use! resp = httpClient.SendAsync(req, HttpCompletionOption.ResponseHeadersRead)
+        let exists = File.Exists path
 
-        if not resp.IsSuccessStatusCode then
-            failwith $"Failed to fetch input for day {day}"
+        if not exists || force then
+            use req = new HttpRequestMessage(HttpMethod.Get, url)
+            req.Headers.Add("Cookie", $"session={session}")
+            req.Headers.Add("User-Agent", "Itsbth-AoC/0.1 (github.com/itsbth/aoc-2024-fsharp)")
+            use! resp = httpClient.SendAsync(req, HttpCompletionOption.ResponseHeadersRead)
 
-        use! stream = resp.Content.ReadAsStreamAsync()
-        use fileStream = new FileStream(path, FileMode.Create, FileAccess.Write)
-        do! stream.CopyToAsync(fileStream)
+            if not resp.IsSuccessStatusCode then
+                return Error $"Failed to fetch input: {resp.StatusCode}"
+            else
+                use! stream = resp.Content.ReadAsStreamAsync()
+                use fileStream = new FileStream(path, FileMode.Create, FileAccess.Write)
+                do! stream.CopyToAsync(fileStream)
+                return Success
+        else
+            return Exists
     }
 
-// assume december :)
-let today = DateTime.Today.Day
+type args = { day: int; force: bool; help: bool }
 
-let day =
-    match fsi.CommandLineArgs with
-    // first is script path
-    | [| _; d |] -> int d
-    | _ -> today
+let parseArgs cli =
+    let rec loop args =
+        function
+        | [] -> args
+        | "-d" :: day :: rest -> loop { args with day = int day } rest
+        | "-f" :: rest -> loop { args with force = true } rest
+        | "-h" :: _ -> { args with help = true }
+        | _ :: rest -> loop args rest
 
-printfn $"Fetching input for day %d{day}"
-fetchInput day |> Async.AwaitTask
-printfn "Done"
+    loop
+        { day = DateTime.Today.Day
+          force = false; help = false }
+        cli
+
+let args = parseArgs <| List.ofArray fsi.CommandLineArgs
+
+printfn $"Fetching input for day %d{args.day}"
+if args.help then
+    printfn $"Usage: GetInput [-d <day>] [-f] [-h]"
+    printfn $"-d <day>  Day to fetch input for (default: %d{DateTime.Today.Day})"
+    printfn $"-f        Force overwrite of existing input"
+    printfn $"-h        Display this help message"
+else
+    match fetchInput args.day args.force |> Async.AwaitTask |> Async.RunSynchronously with
+    | Success -> printfn "Done"
+    | Exists -> printfn "Input already exists"
+    | Error e -> printfn $"Error: {e}"
